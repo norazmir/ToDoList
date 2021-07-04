c183bf1a5ed3c7ed0053fd8580e03e4b599f0d02
DESKTOP-VEII6NF\fireblaze <mdnorazmir@gmail.com>
2021-07-04 17:22:10 +0800

    1. ViewBinding with Adapter 2. Comment some android library to fix build issue

--- app/src/main/java/com/norazmir/todolist/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/norazmir/todolist/ui/tasks/TasksFragment.kt
@@ -1,12 +1,37 @@
 package com.norazmir.todolist.ui.tasks
 
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.norazmir.todolist.R
+import com.norazmir.todolist.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel:TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val tasksAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner){
+            tasksAdapter.submitList(it)
+        }
+    }
 }
