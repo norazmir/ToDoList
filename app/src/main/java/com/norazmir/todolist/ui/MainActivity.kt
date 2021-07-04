061e92a4bc4f45a0601b4976f8ae5613d9bdc090
DESKTOP-VEII6NF\fireblaze <mdnorazmir@gmail.com>
2021-07-04 13:44:31 +0800

    adding up tasks fragment

--- app/src/main/java/com/norazmir/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/norazmir/todolist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.norazmir.todolist
+package com.norazmir.todolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.norazmir.todolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/norazmir/todolist/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.norazmir.todolist.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.norazmir.todolist.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
