ce05652f7df09cdef6e5a52d577e46caeff024e0
DESKTOP-VEII6NF\fireblaze <mdnorazmir@gmail.com>
2021-07-04 23:59:14 +0800

    Combining multiple flows

--- app/src/main/java/com/norazmir/todolist/data/TaskDao.kt
+++ app/src/main/java/com/norazmir/todolist/data/TaskDao.kt
@@ -1,14 +1,24 @@
 package com.norazmir.todolist.data
 
 import androidx.room.*
+import com.norazmir.todolist.ui.tasks.SortOrder
 import dagger.multibindings.IntoSet
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
-    @Query("select * from task_table where name like '%' || :searchQuery || '%' order by important desc")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder){
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("select * from task_table where (completed != :hideCompleted or completed = 0) and name like '%' || :searchQuery || '%' order by important desc, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("select * from task_table where (completed != :hideCompleted or completed = 0) and name like '%' || :searchQuery || '%' order by important desc, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
